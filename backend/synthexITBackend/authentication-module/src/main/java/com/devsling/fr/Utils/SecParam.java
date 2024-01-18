0ad2db73d08ae39c6014309f02215be2ab296263
said <mohamed.s.taleb@devsling.com>
2024-01-18 13:41:50 -0500

    [feature]:Implement authentication between APIGateway and Keycloak Resource Server

--- backend/synthexITBackend/authentication-module/src/main/java/com/devsling/fr/Utils/SecParam.java
+++
@@ -1,7 +0,0 @@
-package com.devsling.fr.Utils;
-
-public interface SecParam {
-    public static final String Secret = "mohamedsaidtaleb8@gmail.com";
-    public static final String Prefixe = "Bearer ";
-
-}
