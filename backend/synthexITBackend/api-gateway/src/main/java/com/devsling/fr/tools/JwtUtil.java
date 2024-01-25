f84e8035889ae1760ce3326058df0b9e3f0f1dc3
said <mohamed.s.taleb@devsling.com>
2024-01-25 16:27:39 -0500

    [feature:exception-Handler in the gateway]:global exception handler

--- backend/synthexITBackend/api-gateway/src/main/java/com/devsling/fr/tools/JwtUtil.java
+++ backend/synthexITBackend/api-gateway/src/main/java/com/devsling/fr/tools/JwtUtil.java
@@ -1,11 +1,14 @@
 package com.devsling.fr.tools;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -16,7 +19,26 @@ public class JwtUtil {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
     }
 
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
 
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaim(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaim(String token) {
+        return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+    }
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
 
     private Key getSignKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);
