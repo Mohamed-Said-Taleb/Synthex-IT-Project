e13cf3eaa669cee5b2ad890362737d773dc3be0d
said <mohamed.s.taleb@devsling.com>
2024-01-31 14:00:35 -0500

    Add global exception handler &Configure WebClient for service communication

--- backend/synthexITBackend/candidate-service/src/main/java/com/devsling/fr/tools/Constants.java
+++ backend/synthexITBackend/candidate-service/src/main/java/com/devsling/fr/tools/Constants.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Constants {
-    public static final String AUTH_SERVICE = "auth-service";
     public static final String APPLICATION_JSON_UTF8_VALUE = "application/json;charset=UTF-8";
 
 
